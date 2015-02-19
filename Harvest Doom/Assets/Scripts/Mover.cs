869800785c8f3a3731c4508466c0d7310ec429cc
Funkshaz <kylanvike@gmail.com>
2015-02-18 21:16:08 -0700

    Second commit.

---
+++ Harvest Doom/Assets/Scripts/Mover.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+	
+	public float movespeed;
+	
+		void Start () {
+
+	}
+
+
+		void Update () {
+
+			if(Input.GetKey(KeyCode.A))
+		{
+			transform.Translate(new Vector3 (-movespeed, 0, 0) * Time.deltaTime);
+		}
+			if(Input.GetKey(KeyCode.D))
+		{
+			transform.Translate(new Vector3 (movespeed, 0, 0) * Time.deltaTime);
+		}
+			if(Input.GetKey(KeyCode.W))
+		{
+			transform.Translate(new Vector3 (0, movespeed, 0) * Time.deltaTime);
+		}
+			if(Input.GetKey(KeyCode.S))
+		{
+			transform.Translate(new Vector3 (0, -movespeed, 0) * Time.deltaTime);
+		}
+	}
+}
